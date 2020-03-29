2b4abdc85dd715604b832d484b408bf1ed6f3063
Guilherme Schweizer <guilherme.schweizer@gmail.com>
2020-03-29 15:39:03 -0300

    [STORY-1] Removed useless parser

--- src/api.js
+++ src/api.js
@@ -1,12 +1,10 @@
 import axios from 'axios'
 import configuration from './configuration'
-import { parseDataToCamelCase } from './Parser'
 
 const get = resource =>
   axios
     .get(`${configuration.apiUrl}/${resource}`)
     .then(response => response.data)
-    .then(response => parseDataToCamelCase(response))
 
 export const getAllUserRepos = username =>
   get(`users/${username}/repos`).then(response => response)
