bdce2b3d29e2b4225998d3aeea4b6e6b1b014590
Guilherme Schweizer <guilherme.schweizer@gmail.com>
2020-04-01 22:26:35 -0300

    [STORY-1] Changed URL to get starred repositories instead user repositories

--- src/api.js
+++ src/api.js
@@ -7,7 +7,7 @@ const get = resource =>
     .then(response => response.data)
 
 export const getAllUserRepos = username =>
-  get(`users/${username}/repos`).then(response => response)
+  get(`users/${username}/starred`).then(response => response)
 
 export const getRepoLanguages = languagesUrl =>
   get(languagesUrl).then(response => response.data)
