ef137365f8e718456db27079d21cd4a4c3cd8477
Guilherme Schweizer <guilherme.schweizer@gmail.com>
2020-04-06 22:23:55 -0300

    [STORY-1] Created initial loading style

--- src/Loader/index.js
+++ src/Loader/index.js
@@ -1,10 +1,19 @@
 import React from 'react'
 
+import './loader.sass'
+
 const Loader = () => (
   <div>
     <h2 className="main-title">GitHub Stars</h2>
-
-    <h1>IT'S LOADING</h1>
+    <div className="loader">
+      <div className="loading-animation">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+      <h4>Getting the repositories list from Github...</h4>
+    </div>
   </div>
 )
 
